8023d3192942dc8d4f08e3ddda8eefb60655c302
Ishmam.Haque <ishmam.haque@bjitgroup.com>
2020-05-10 22:08:51 +0600

    first commit

---
+++ objects/catetoryRelations.php
@@ -0,0 +1,32 @@
+<?php
+class CatetoryRelations{
+ 
+    // database connection and table name
+    private $conn;
+    private $table_name = "category";
+ 
+    // object properties
+    public $Id;
+    public $UUID;
+    public $categoryId;
+    public $ParentcategoryId;
+
+ 
+    public function __construct($db){
+        $this->conn = $db;
+    }
+ 
+    // used by select drop-down list
+    function readAll(){
+        //select all data
+        $query = "select category.Id as id,ParentcategoryId as parent_id from category left join catetory_relations
+on category.Id = catetory_relations.categoryId order by ParentcategoryId desc";  
+ 
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+ 
+        return $stmt;
+    }
+ 
+}
+?>
---
+++ task2.php
@@ -0,0 +1,66 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+	include_once 'config/database.php';
+	include_once 'objects/catetoryRelations.php';
+	 
+	$database = new Database();
+	$db = $database->getConnection();
+	 
+	$CatetoryRelations = new CatetoryRelations($db);
+	$stmt = $CatetoryRelations->readAll();
+
+	$page_title = "Task2";
+	include_once "layout_header.php";
+
+	$items = [];
+	while ($row = $stmt->fetch(PDO::FETCH_OBJ)){
+		$items[] = $row;
+		// var_dump($row);exit();
+	}
+
+
+$childs = array();
+
+foreach($items as $item)
+    $childs[$item->ParentcategoryId][] = $item;
+
+foreach($items as $item) if (isset($childs[$item->categoryId]))
+    $item->childs = $childs[$item->categoryId];
+
+$tree = $childs;
+
+
+?>
+	
+
+<!-- <ul id="myUL">
+  <li><span class="caret">Beverages</span>
+    <ul class="nested">
+      <li>Water</li>
+      <li>Coffee</li>
+      <li><span class="caret">Tea</span>
+        <ul class="nested">
+          <li>Black Tea</li>
+          <li>White Tea</li>
+          <li><span class="caret">Green Tea</span>
+            <ul class="nested">
+              <li>Sencha</li>
+              <li>Gyokuro</li>
+              <li>Matcha</li>
+              <li>Pi Lo Chun</li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </li>
+</ul> -->
+
+ <?php
+include_once "layout_footer.php";
+?>
+
+<s
