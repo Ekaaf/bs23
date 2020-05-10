c6c50f91e0f701aabfab708736268a115bdfb66d
Ishmam.Haque <ishmam.haque@bjitgroup.com>
2020-05-10 22:38:30 +0600

    first commit

--- objects/item.php
+++ objects/item.php
@@ -23,8 +23,13 @@ class Item{
     // used by select drop-down list
     function getCount(){
         //select all data
-        $query = "select category.Id as categoryId,ParentcategoryId from category left join catetory_relations
-on category.Id = catetory_relations.categoryId order by ParentcategoryId";  
+        $query = "SELECT count(*) as relID, cat.id as catID, cat_rel.parentName as categoryId, cat.Name as name
+            FROM item_category_relations rel 
+        inner join category cat on rel.categoryId = cat.id
+        left join Item on rel.ItemNumber = Item.Number
+        left join (select catetory_relations.id as cat_rel_id, catetory_relations.ParentcategoryId, catetory_relations.categoryId, category.name as parentName from catetory_relations left join category on catetory_relations.ParentcategoryId =category.id) cat_rel on cat.id = cat_rel.categoryId
+        GROUP BY catID
+        ORDER BY catID";  
  
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
