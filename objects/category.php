7ceddefb7098e514183e7a77de52fb1d01cad2fc
Ishmam.Haque <ishmam.haque@bjitgroup.com>
2020-05-11 05:04:50 +0600

    first commit

---
+++ objects/category.php
@@ -0,0 +1,55 @@
+<?php
+class Category{
+ 
+    // database connection and table name
+    private $conn;
+    private $table_name = "category";
+ 
+    // object properties
+    public $Id;
+    public $UUID;
+    public $categoryId;
+    public $ParentcategoryId;
+
+ 
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+
+    function getCategoriesList(){
+        //select all data
+        $query = "SELECT * FROM category";  
+ 
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+ 
+        return $stmt;
+    }
+ 
+    function getCountByCategory(){
+        //select all data
+        $query = "select t1.ParentcategoryId as lev1, t1.categoryId as lev2, t2.categoryId as lev3,t3.categoryId as lev4,
+            t4.categoryId as lev5, t5.categoryId as lev6, t6.categoryId as lev7, cat, total
+            from catetory_relations as t1 
+            left join catetory_relations as t2 on t1.categoryId = t2.ParentcategoryId
+            left join catetory_relations as t3 on t2.categoryId = t3.ParentcategoryId
+            left join catetory_relations as t4 on t3.categoryId = t4.ParentcategoryId
+            left join catetory_relations as t5 on t4.categoryId = t5.ParentcategoryId
+            left join catetory_relations as t6 on t5.categoryId = t6.ParentcategoryId
+            LEFT join (SELECT categoryId as cat, count(ItemNumber) as total 
+            FROM bs.Item_category_relations group by categoryId)
+            as test
+            on t6.categoryId = test.cat OR t5.categoryId = test.cat OR t4.categoryId = test.cat 
+            OR t3.categoryId = test.cat OR t2.categoryId = test.cat OR t1.categoryId = test.cat
+            OR t1.ParentcategoryId = test.cat 
+            order by lev1,lev2,lev3,lev4,lev5,lev6,lev7";  
+ 
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+ 
+        return $stmt;
+    }
+ 
+}
+?>
--- task2.php
+++ task2.php
@@ -1,39 +1,101 @@
 <?php
 	
 	include_once 'config/database.php';
-	include_once 'objects/catetoryRelations.php';
+	include_once 'objects/category.php';
 	 
 	$database = new Database();
 	$db = $database->getConnection();
-	 
-	$CatetoryRelations = new CatetoryRelations($db);
-	$stmt = $CatetoryRelations->readAll();
-
+	
 	$page_title = "Task2";
 	include_once "layout_header.php";
 
+	$Category = new Category($db);
+	$categoriesStmt = $Category->getCategoriesList();
+	$categories = [];
+	while ($row = $categoriesStmt->fetch(PDO::FETCH_ASSOC)){
+		$categories[$row['Id']] = $row['Name'];
+	}
+
+	$stmt = $Category->getCountByCategory();
 	$items = [];
-	while ($row = $stmt->fetch(PDO::FETCH_OBJ)){
+	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
 		$items[] = $row;
 	}
 
-print_r($items);
-$childs = array();
-
-foreach($items as $item)
-    $childs[$item->ParentcategoryId][] = $item;
-
-foreach($items as $item) if (isset($childs[$item->categoryId]))
-    $item->childs = $childs[$item->categoryId];
-
-$tree = $childs;
-
-
+$data = array_slice($items, 0,5);
+$data = $items;
+// var_dump(count($data));exit();
 ?>
 	
+<h2>Please click to open</h2>
+<ul id="myUL">
+	<?php
+		$prevLev1 = ''; 
+		$prevLev2 = '';
+		$prevLev3 = '';
+		$prevLev4 = '';
+		$prevLev5 = '';
+		$prevLev6 = '';
+		$prevLev7 = '';
+		$check1 = 0;$check2 = 0;$check3 = 0;$check4 = 0;$check1 = 0;$check1 = 0;$check1 = 0;
+		foreach ($data as $d) {
+	?>
+  	<?php
+		  $li = ""; 
+			if(!is_null($d['lev2'])){
+				if($prevLev1!=$d['lev1']){
+					$prevLev1 = $d['lev1'];
+					if($check2==1){
+						$li .= '</ul>';
+					}
+					$li .= '<li><span class="caret">'.$categories[$d['lev1']].'1</span><ul class="nested">';
+					$check2 = 0;
+				}
+				
+				if(!is_null($d['lev3'])){
+					if($prevLev2!=$d['lev2']){
+						$prevLev2 = $d['lev2'];
+						if($check3==1){
+							$li .= '</ul>';
+						}
+						$li .= '<li><span class="caret">'.$categories[$d['lev2']].'2</span><ul class="nested">';
+						$check3 = 0;
+					}
+					else{
+						$check3 = 1;
+						$li .='<li>'.$categories[$d['lev3']].'3</li>';
+					}
+					if(!is_null($d['lev4']) ){
+						if($prevLev3!=$d['lev3']){
+							$prevLev3 = $d['lev3'];
+							if($check4==1){
+								$li .= '</ul>';
+							}
+							$li .= '<li><span class="caret">'.$categories[$d['lev3']].'</span><ul class="nested">';
+							$check4 = 0;
+						}
+						else{
+						$check4 = 1;
+							$li .='<li>'.$categories[$d['lev3']].'3</li>';
+						}
+					}
+				}
+				else{
+					$li .='<li>'.$categories[$d['lev2']].'2</li>';
+				}
+				// else{
+				// 	$li .= '</li>'
+				// }
+			}
 
-<!-- <ul id="myUL">
-  <li><span class="caret">Beverages</span>
+			echo $li;
+		?>
+  <?php } ?>
+</ul>
+
+<br><br>
+<ul id="myUL">
+  <li><span class="caret">aaa</span>
     <ul class="nested">
       <li>Water</li>
       <li>Coffee</li>
@@ -53,7 +115,7 @@ $tree = $childs;
       </li>
     </ul>
   </li>
-</ul> -->
+</ul>
 
  <?php
 include_once "layout_footer.php";
